fdf4ff62de665eb881c3c1c86fe95906d645488a
Wilken Rivera <dev@wilkenrivera.com>
2021-07-01 17:57:02 -0400

    Add shared client files that were updated when generating the packer-service client

--- clients/cloud-shared/v1/models/hashicorp_cloud_common_pagination_request.go
+++ clients/cloud-shared/v1/models/hashicorp_cloud_common_pagination_request.go
@@ -6,6 +6,8 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"context"
+
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -34,6 +36,11 @@ type HashicorpCloudCommonPaginationRequest struct {
 
 // Validate validates this hashicorp cloud common pagination request
 func (m *HashicorpCloudCommonPaginationRequest) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// ContextValidate validates this hashicorp cloud common pagination request based on context it is used
+func (m *HashicorpCloudCommonPaginationRequest) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
 	return nil
 }
 
