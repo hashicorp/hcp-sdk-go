c0a0fd4408c5d41db333230f9e9abcddc2b27cf8
Chris Chapman <cchapman@hashicorp.com>
2022-03-10 21:17:29 -0800

    Demoing picking up orgs/projects from the api vs setting variables

--- cmd/hcp-sdk-go-client/main.go
+++ cmd/hcp-sdk-go-client/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
 	consul "github.com/hashicorp/hcp-sdk-go/clients/cloud-consul-service/preview/2020-08-26/client/consul_service"
 	network "github.com/hashicorp/hcp-sdk-go/clients/cloud-network/preview/2020-09-07/client/network_service"
+	orgs "github.com/hashicorp/hcp-sdk-go/clients/cloud-resource-manager/preview/2019-12-10/client/organization_service"
+	projs "github.com/hashicorp/hcp-sdk-go/clients/cloud-resource-manager/preview/2019-12-10/client/project_service"
 	"github.com/hashicorp/hcp-sdk-go/httpclient"
 )
 
@@ -18,13 +19,26 @@ func main() {
 		log.Fatal(err)
 	}
 
+	org := orgs.New(cl, nil)
+	respOrg, err := org.OrganizationServiceList(orgs.NewOrganizationServiceListParams(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	orgID := respOrg.Payload.Organizations[0].ID
+
+	proj := projs.New(cl, nil)
+	projParams := projs.NewProjectServiceListParams()
+	scopeType := "ORGANIZATION"
+	projParams.SetScopeType(&scopeType)
+	projParams.SetScopeID(&orgID)
+	respProj, err := proj.ProjectServiceList(projParams, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	projID := respProj.Payload.Projects[0].ID
 	// Import versioned client for each service.
 	consulClient := consul.New(cl, nil)
 	networkClient := network.New(cl, nil)
-
-	// These IDs can be obtained from the portal URL
-	orgID := os.Getenv("HCP_ORGANIZATION_ID")
-	projID := os.Getenv("HCP_PROJECT_ID")
 
 	// Exercise the versioned clients via a single http client,
 	// rather than initializing a new http client per service.
