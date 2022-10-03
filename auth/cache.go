d813e3372b849627fef2f84c3bbf73d01584a34d
Brenna Hewer-Darroch <21015366+bcmdarroch@users.noreply.github.com>
2022-10-03 15:37:16 -0400

    pseudocode plan for cache package

---
+++ auth/cache.go
@@ -0,0 +1,43 @@
+package auth
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"os"
+// )
+
+// type cache struct
+// access token
+// refresh token
+// expiration time
+// max age
+
+// 1. Write to json file in a specific location in home directory (OS-dependent)
+
+// Setter for access_token
+
+// Setter for refresh_token
+
+// Setter for expiration time
+
+// 2. Read from json file in a specific location in home directory (OS-dependent)
+
+// Getter for access_token
+
+// Getter for refresh_token
+
+// Getter for expiration time
+
+// Constant for max age - 24 hours
+
+// Constant for file name {HOME}"/hcp/credentials.json"
+
+// 3. helpers
+
+// check if directory exists
+
+// create directory + file
+
+// tokenToJson
+
+// JsonToToken
