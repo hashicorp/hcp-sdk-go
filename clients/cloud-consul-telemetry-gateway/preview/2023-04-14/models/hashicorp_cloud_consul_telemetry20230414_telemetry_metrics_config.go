dbc23530f515d0addd75a33fce9d5a382d528e30
HashiCorp Cloud Services <hashicorp-cloud@hashicorp.com>
2023-08-28 15:53:07 +0000

    Update cloud-consul-telemetry-gateway SDK

--- clients/cloud-consul-telemetry-gateway/preview/2023-04-14/models/hashicorp_cloud_consul_telemetry20230414_telemetry_metrics_config.go
+++ clients/cloud-consul-telemetry-gateway/preview/2023-04-14/models/hashicorp_cloud_consul_telemetry20230414_telemetry_metrics_config.go
@@ -17,6 +17,9 @@ import (
 // swagger:model hashicorp.cloud.consul_telemetry_20230414.TelemetryMetricsConfig
 type HashicorpCloudConsulTelemetry20230414TelemetryMetricsConfig struct {
 
+	// Disabled is a bool used to indicate whether metrics pushing from the server or proxy disabled
+	Disabled bool `json:"disabled,omitempty"`
+
 	// Endpoint is the URL to use when exporting metrics via OTLP. If set will override the global endpoint in TelemetryConfig
 	Endpoint string `json:"endpoint,omitempty"`
 
