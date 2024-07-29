0f4f5265e19df977fd3b6861dc75db85d2126532
Liam Cervante <liam.cervante@hashicorp.com>
2024-07-29 11:32:03 +0200

    stacks: add support for authenticating directly via token (#262)
    * stacks: add support for authenticating directly via token
    
    * add basic test and changelog entry

---
+++ auth/workload/token_test.go
@@ -0,0 +1,31 @@
+package workload
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCredentialTokenSource_Validate(t *testing.T) {
+	tests := []struct {
+		name   string
+		ct     *CredentialTokenSource
+		errStr string
+	}{
+		{
+			name:   "empty token",
+			ct:     &CredentialTokenSource{},
+			errStr: "token must be set",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := test.ct.Validate()
+			if test.errStr == "" {
+				require.NoError(t, err)
+			} else {
+				require.ErrorContains(t, err, test.errStr)
+			}
+		})
+	}
+}
